e60f69c709778ffba941db022cc2a367450f0fe6
miki1209-dev <crest0421@gmail.com>
2025-06-10 14:33:40 +0900

    Fix SCSS entry path for Heroku build

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 	<!-- Viteの読み込み -->
-	@vite(['resources/css/style.scss', 'resources/js/app.js'])
+	@vite(['resources/scss/style.scss', 'resources/js/app.js'])
 	<!-- Font Awesome -->
 	<script src="https://kit.fontawesome.com/1843201c14.js" crossorigin="anonymous"></script>
 	{{-- Stripe Elements --}}

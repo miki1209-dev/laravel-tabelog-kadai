753f07d66eb485831318198cd021310cd5d49cb8
miki1209-dev <crest0421@gmail.com>
2025-06-11 09:32:10 +0900

    UserSeeder追加

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$user = new User();
+		$user->name = 'testConfirmation@example.com';
+		$user->email_verified_at = Carbon::now();
+		$user->password = Hash::make('password');
+		$user->postal_code = "0000000";
+		$user->address = "東京都";
+		$user->phone = "000-0000-0000";
+		$user->save();
+	}
+}
